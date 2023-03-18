5ffd1e9be2fd70c1a39975d5ea281740ed417592
Abdeljaouad Mezrari <mac@MacBook-Pro-de-mac.local>
2023-03-18 12:51:23 +0100

    Init I am rich Project

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Abdeljaouad Mezrari on 25/02/2023.
+//  Copyright © 2023 Abdeljaouad Mezrari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
